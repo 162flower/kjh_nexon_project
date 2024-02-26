75c84c13815a7936681b3b10e49d0177f155b4ce
162flower <162flower@naver.com>
2024-02-26 09:16:05 +0900

    240226 넥슨 프로젝트

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.main_slide').slick({
+    prevArrow : false,
+    nextArrow : false,
+    dots : true,
+    appendDots : '.m1_pager',
+    autoplay : true,
+    autoplaySpeed : 2000,
+    speed : 1200
+ });
+
+
+
